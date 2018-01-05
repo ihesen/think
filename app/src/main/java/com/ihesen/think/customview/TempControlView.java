7d598e93ba87f047af3fc1701c04bedc2bb4fbd1
hesen <Ibeiquan@2017>
2018-01-05 17:46:49 +0800

    自定义view

---
+++ app/src/main/java/com/ihesen/think/customview/TempControlView.java
@@ -0,0 +1,13 @@
+package com.ihesen.think.customview;
+import android.view.View;
+
+
+/**
+ * 类名
+ * @author hesen
+ *	实现的主要功能：
+ *	创建日期 2018/1/4
+*	修改者，修改日期，修改内容。
+*/
+public    class TempControlView extends View   {
+}
